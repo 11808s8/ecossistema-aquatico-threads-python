f9ce74d7e692c67dc7b5b9c7ace23698f468e395
Adriano Gomes <adrianogsss@gmail.com>
2019-10-20 10:53:07 -0300

    feat: tubarões podem comer peixes agora

--- tubarao.py
+++ tubarao.py
@@ -3,5 +3,5 @@ from animal import Animal
 class Tubarao(Animal):
     
     def __init__(self, id,imagem, rect, altura, largura, x=0 ,y=0,calorias=600):
-        super().__init__( id,imagem, rect, altura, largura, x,y,calorias, ['foca'])
+        super().__init__( id,imagem, rect, altura, largura, x,y,calorias, ['foca','peixe'])
     
