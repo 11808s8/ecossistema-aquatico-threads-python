5a3065e3dcb7094a33a85162ce01031cdf0fb99e
Adriano Gomes <adrianogsss@gmail.com>
2019-11-29 18:01:23 -0300

    feat: separação em funções específicas de atualização de tempo e chamada para

--- tela.py
+++ tela.py
@@ -36,12 +36,9 @@ class Tela(threading.Thread):
                 proximo_semaforo = random.randint(0,(len(utils.semaforos)-2))
         
         
-            for i in range(utils.tamanho_matriz):
-                print(utils.mundo[i])
+            self.__atualiza_tempos__()
+            self.__tratamento_fome_mundo__()
             
-            
-            utils.decrementa_calorias(int(self.time_since_enter), int(self.time_since_enter_backup))
-            utils.limpa_mortos_de_fome()
             self.__exibe_mundo__(self.screen)
             
             if(utils.mundo_vazio() or utils.apenas_algas()):
@@ -50,9 +47,9 @@ class Tela(threading.Thread):
                     utils.semaforos[i].release()
                 break
             
-            # print("Chama o próximo: " + str(proximo_semaforo))
+            time.sleep(0.1)
             utils.semaforos[proximo_semaforo].release()
-            # time.sleep(1)
+            
 
     @staticmethod    
     def leitura_quit(events,release_semaforos=False):
@@ -63,10 +60,15 @@ class Tela(threading.Thread):
                 if(release_semaforos):
                     # libera todos os locks
                     for i in range(len(utils.semaforos)):
-                        # print("liberando " + str(i))
                         utils.semaforos[i].release()
                     break
     
+    def __tratamento_fome_mundo__(self):
+        if((int(self.time_since_enter) - int(self.time_since_enter_backup))>=1):
+            utils.decrementa_calorias(self.time_since_enter, self.time_since_enter_backup)
+        utils.limpa_mortos_de_fome()
+    
+
     def __exibe_mundo__(self, screen):
         screen.fill(utils.COR_MAR)
         for x in range(utils.tamanho_matriz):
@@ -76,9 +78,6 @@ class Tela(threading.Thread):
         
         seres_na_tela = utils.contador_seres_no_mundo()
 
-        self.time_since_enter_backup = self.time_since_enter
-
-        self.time_since_enter = self.__tempo_desde_start_no_jogo__()
         message = 'Tempo transcorrido: ' + '%i'%(self.time_since_enter)
         screen.blit(utils.FONT.render(message, True, utils.TEXT_COLOR), (0, 0))
         
@@ -88,5 +87,10 @@ class Tela(threading.Thread):
         
         pygame.display.flip()
     
+    def __atualiza_tempos__(self):
+        self.time_since_enter_backup = self.time_since_enter
+        self.time_since_enter = self.__tempo_desde_start_no_jogo__()
+
+
     def __tempo_desde_start_no_jogo__(self):
         return ((pygame.time.get_ticks() - self.start_time)/1000)
