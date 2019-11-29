e0a0aab29b89421a8e06c6f0cdee8d4faa15a3c5
Adriano Gomes <adrianogsss@gmail.com>
2019-11-29 18:02:32 -0300

    feats: Otimização, limpeza e comentários no código

--- test.py
+++ test.py
@@ -7,21 +7,14 @@ from alga import Alga
 from tubarao import Tubarao
 from peixe import Peixe
 import random
-# from threading import Semaphore
 from tela import Tela
 from utils import utils
 
 pygame.init()
 pygame.display.set_caption('Ecossistema Aquático')
-# semaforo = Semaphore()
-# print(utils.mundo)
 # Para o TEMPO
 
-
 clock = pygame.time.Clock()
-
-# @TODO: Colocar um START_TIME para quando o USUÁRIO decidir iniciar o game
-
 
 seres_objetos = []
 inputs = [0 for i in range(len(utils.seres))]
@@ -30,11 +23,8 @@ textinputs = [pygame_textinput.TextInput("","",35,True,(255,255,0)) for i in ran
 calorias_input = pygame_textinput.TextInput("","",35,True,(255,255,0))
 screen = pygame.display.set_mode((utils.w, utils.h))
 calorias = 600
-# textinput.update(events)
-
-
-        
-        # Exibe a menssagem na tela
+    
+# Exibe a menssagem na tela
 while True:
     events = pygame.event.get()
     for event in events:
@@ -46,12 +36,12 @@ while True:
             break
     screen.fill((0,0,0))
     message = 'Digite quantas calorias você quer por animal na simulação:'
-    screen.blit(utils.FONT.render(message, True, utils.TEXT_COLOR), (0,(utils.h/2)))
-    screen.blit(calorias_input.get_surface(),(utils.FONT.size(message)[0],(utils.h/2)))
+    screen.blit(utils.FONT.render(message, True, utils.TEXT_COLOR), (40,(utils.h/2)))
+    screen.blit(calorias_input.get_surface(),(utils.FONT.size(message)[0]+40,(utils.h/2)))
     pygame.display.flip()
     clock.tick(100)
-# Lê entradas
 
+# Lê entradas
 for i,textinput in enumerate(textinputs):
     while True:
         events = pygame.event.get()
@@ -67,9 +57,9 @@ for i,textinput in enumerate(textinputs):
         message = 'Número de %s para a execução da simulação:'%(utils.seres_plural[i])
         
         # Exibe a menssagem na tela
-        screen.blit(utils.FONT.render(message, True, utils.TEXT_COLOR), (0,(utils.h/2)))
+        screen.blit(utils.FONT.render(message, True, utils.TEXT_COLOR), (40,(utils.h/2)))
         # Exibe input logo após a mensagem na tela
-        screen.blit(textinput.get_surface(),(utils.FONT.size(message)[0],(utils.h/2)))
+        screen.blit(textinput.get_surface(),(utils.FONT.size(message)[0]+40,(utils.h/2)))
         pygame.display.flip()
         clock.tick(100)
 
@@ -97,6 +87,10 @@ for quantidade_seres in inputs:
     for j in range(quantidade_seres):
         total_seres+=1
 
+if(total_seres > (utils.tamanho_matriz*utils.tamanho_matriz)):
+    print("-- Tamanho de seres excede o tamanho do tabuleiro! --")
+    exit()
+
 utils.inicializa_semaforos(total_seres)
 print(utils.semaforos)
 # quit()
@@ -109,34 +103,33 @@ for chave,quantidade_seres in enumerate(inputs):
         # (x, y) = utils.coloca_em_posicao_aleatoria(ids,utils.seres[chave])
         (x, y) = utils.coloca_em_posicao_aleatoria(None)
         if(utils.seres[chave] == 'alga'):
-            novo_ser = Alga(ids,alga,alga.get_rect(),10,20,x,y)
+            novo_ser = Alga(ids,alga,alga.get_rect(),utils.tamanho_sprite,utils.tamanho_sprite,x,y)
         elif(utils.seres[chave] == 'peixe'):
-            novo_ser = Peixe(ids,peixe,peixe.get_rect(),10,20,x,y,calorias)
+            novo_ser = Peixe(ids,peixe,peixe.get_rect(),utils.tamanho_sprite,utils.tamanho_sprite,x,y,calorias)
         elif(utils.seres[chave] == 'tubarao'):
-            novo_ser = Tubarao(ids,tubarao,tubarao.get_rect(),10,20,x,y,calorias)
+            novo_ser = Tubarao(ids,tubarao,tubarao.get_rect(),utils.tamanho_sprite,utils.tamanho_sprite,x,y,calorias)
         elif(utils.seres[chave] == 'foca'):
-            novo_ser = Foca(ids,foca,foca.get_rect(),10,20,x,y,calorias)
+            novo_ser = Foca(ids,foca,foca.get_rect(),utils.tamanho_sprite,utils.tamanho_sprite,x,y,calorias)
         # novo_ser.exibe(screen)
         utils.coloca_em_posicao_especifica(novo_ser)
         seres_objetos.append(novo_ser)
+        
+        # Incrementa os IDS para definir qual será o semáforo da tela (último)
         ids+=1
-# pygame.display.flip()
-# input()
-# print(utils.mundo)
-# quit()
+        
 
 start_time = pygame.time.get_ticks()
 
 tela = Tela(ids, screen, start_time)
-print(" ID TELA " + str(ids))
-print("Semaforo liberado: " + str((len(utils.semaforos)-1)))
-# utils.semaforos[(len(utils.semaforos)-1)].release()
+
+# Inicialização de threads
 for ser in seres_objetos:
     ser.start()
-print(seres_objetos)
 tela.start()
+
+# Fechamento de threads
 for ser in seres_objetos:
     ser.join()
-
 tela.join()
-quit()
+
+# quit()
